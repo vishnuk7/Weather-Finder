75a30c8d698f0d74b6e857c2634200abe8adc79c
viu <vishnu.kraman88@gmail.com>
2018-06-18 23:51:11 +0530

    weather-icon

--- public/src/components/WeatherContent.js
+++ public/src/components/WeatherContent.js
@@ -5,11 +5,19 @@ const WeatherContent = (props)=>(
     <section>
         <p>WeatherContent</p>
         {props.state.error && <p>{props.state.error}</p>}
-        {props.state.temperature && <p>Temperature:{props.state.temperature}</p>}
-        {props.state.city && <p>City:{props.state.city}</p>}
-        {props.state.country && <p>Country:{props.state.country}</p>}
-        {props.state. humidity && <p>Humidity:{props.state.humidity}</p>}
-        {props.state.description && <p>Description:{props.state.description}</p>}
+        <div className="row">
+            <div className="col-1-of-2">
+                {props.state.temperature && <p>Temperature:{props.state.temperature}</p>}
+                {props.state.description && <p>Description:{props.state.description}</p>}
+            </div>
+
+            <div className="col-1-of-2">
+                {props.state.city && <p>City:{props.state.city}</p>}
+                {props.state.country && <p>Country:{props.state.country}</p>}
+                {props.state. humidity && <p>Humidity:{props.state.humidity}</p>}
+            </div>
+
+        </div>
     </section>
 );
 

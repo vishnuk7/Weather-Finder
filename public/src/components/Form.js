a5c699880e5bd3227f1d6bcfdf5f4c16778e4684
viu <vishnu.kraman88@gmail.com>
2018-06-18 21:31:22 +0530

    git pull

--- public/src/components/Form.js
+++ public/src/components/Form.js
@@ -29,7 +29,7 @@ export default class Form extends React.Component {
             country:data.sys.country,
             humidity:data.main.humidity,
             description:data.weather[0].description,
-            // temperature:data.,
+            temperature:data.main.temp,
             error:""
         }));
         }else{
--- public/src/components/WeatherContent.js
+++ public/src/components/WeatherContent.js
@@ -5,6 +5,7 @@ const WeatherContent = (props)=>(
     <section>
         <p>WeatherContent</p>
         {props.state.error && <p>{props.state.error}</p>}
+        {props.state.temperature && <p>Temperature:{props.state.temperature}</p>}
         {props.state.city && <p>City:{props.state.city}</p>}
         {props.state.country && <p>Country:{props.state.country}</p>}
         {props.state. humidity && <p>Humidity:{props.state.humidity}</p>}

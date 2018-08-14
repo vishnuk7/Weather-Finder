820651a2ece52f7f932b5092fc52f4d4c4bfd615
viu <vishnu.kraman88@gmail.com>
2018-08-14 21:03:38 +0530

    improved design

--- public/src/components/Title.js
+++ public/src/components/Title.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
 
-const Title = () =>(
-    <section className="section-title">
-        <h1>Weather Finder</h1>
-        <p>version 1.0</p>
-        <p>Created By:Vishnu</p> 
-    </section>
+const Title = () => (
+  <section className="section-title">
+    <div className="section-content">
+      <h1>Weather Finder</h1>
+      <p>version 1.2</p>
+      <p>Created By:Vishnu</p>
+    </div>
+  </section>
 );
 
 export default Title;
